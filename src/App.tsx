8a0f0e8390b32b0ce1520eded13982dc66336275
Sergii Sokurenko <s.sokurenko@gmail.com>
2021-04-24 22:37:16 +0200

    Added FontAwesome 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faParachuteBox } from '@fortawesome/free-solid-svg-icons'
 import { useConfig } from './hooks'
 
 const App = () => {
-  const { appName, appMotto } = useConfig()
+  const { appName, appMotto, primaryColor } = useConfig()
   return (
     <div>
       <div className="p-6 m-4 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0">!</div>
+        <div className="flex-shrink-0">
+          <FontAwesomeIcon
+            icon={faParachuteBox}
+            size="2x"
+            color={primaryColor}
+          />
+        </div>
         <div>
           <div className="text-xl font-medium text-black">{appName}</div>
           <p className="text-gray-500">{appMotto}</p>
--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -1,7 +1,9 @@
 const useConfig = () => {
   return {
     appName: 'React TypeScript Starter App',
-    appMotto: 'Powered by React, Tailwind CSS'
+    appMotto: 'Powered by React, Tailwind CSS',
+    primaryColor: '#03A9F4',
+    accentColor: '#FF5722'
   }
 }
 
